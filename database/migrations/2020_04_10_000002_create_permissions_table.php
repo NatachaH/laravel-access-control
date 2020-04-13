a82c585b4bb9f3d381b9ba35524bafa0e4ac4db2
Natacha Herth <info@natachaherth.ch>
2020-04-13 17:11:18 +0200

    action default null

--- database/migrations/2020_04_10_000002_create_permissions_table.php
+++ database/migrations/2020_04_10_000002_create_permissions_table.php
@@ -19,7 +19,7 @@ class CreatePermissionsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->string('model')->nullable();
-            $table->string('action')->default('view');
+            $table->string('action')->nullable();
             $table->timestamps();
         });
     }
