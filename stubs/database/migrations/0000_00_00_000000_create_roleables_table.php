02be8759dac88f96910d1eecaec1e889abcfd67d
Natacha Herth <info@natachaherth.ch>
2021-02-23 15:10:00 +0100

    Corr migration

--- stubs/database/migrations/0000_00_00_000000_create_roleables_table.php
+++ stubs/database/migrations/0000_00_00_000000_create_roleables_table.php
@@ -13,7 +13,7 @@ class CreateRoleablesTable extends Migration
      */
     public function up()
     {
-        Schema::table('roleables', function (Blueprint $table)
+        Schema::create('roleables', function (Blueprint $table)
         {
             $table->id();
             $table->bigInteger('role_id')->unsigned();
